c09bfefd5310dbf643fc105c79092fc4cc78a970
Kevin Thomas <me@kevinthomas.dev>
2020-08-05 23:40:31 -0700

    Update layout and add color themes (#27)
    * Change layout, add color theme support
    
    - Modify CSS to be more mobile friendly
    - Modify CSS to support color themes. These themes are configured using
      environment variables that can be passed into the docker container
    - Add support for custom color themes, these are also set through
      environment variables
    
    * Fix ineffectual assignment to
    
    * Set maximum-scale=1 on viewport to prevent mobile safari zoom bug
    
    * Use the flag package for color environment variables
    
    * Check error on fs.Parse

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"log"
+	"os"
 
 	"github.com/namsral/flag"
 	"github.com/prologic/bitcask"
@@ -13,10 +15,17 @@ var (
 
 func main() {
 	var (
-		dbpath         string
-		bind           string
-		maxItems       int
-		maxTitleLength int
+		dbpath               string
+		bind                 string
+		maxItems             int
+		maxTitleLength       int
+		colorTheme           string
+		colorPageBackground  string
+		colorInputBackground string
+		colorForeground      string
+		colorCheckMark       string
+		colorXMark           string
+		colorLabel           string
 	)
 
 	flag.StringVar(&dbpath, "dbpath", "todo.db", "Database path")
@@ -25,12 +34,67 @@ func main() {
 	flag.IntVar(&maxTitleLength, "maxtitlelength", 100, "maximum valid length of a todo item's title")
 	flag.Parse()
 
-	var err error
+	fs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "COLOR", 0)
+	fs.StringVar(&colorTheme, "theme", "dracula", "color theme of the todo list, or 'custom'")
+	fs.StringVar(&colorPageBackground, "pagebackground", "282a36", "page background color")
+	fs.StringVar(&colorInputBackground, "inputbackground", "44475a", "input boxes color")
+	fs.StringVar(&colorForeground, "foreground", "f8f8f2", "text color")
+	fs.StringVar(&colorCheckMark, "check", "50fa7b", "check mark color")
+	fs.StringVar(&colorXMark, "x", "ff5555", "x mark color")
+	fs.StringVar(&colorLabel, "label", "ff79c6", "label color")
+	err := fs.Parse(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	db, err = bitcask.Open(dbpath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	selectColorTheme(colorTheme, colorPageBackground, colorInputBackground, colorForeground,
+		colorCheckMark, colorXMark, colorLabel)
+
 	newServer(bind, maxItems, maxTitleLength).listenAndServe()
+}
+
+func selectColorTheme(colorTheme string, colorPageBackground string, colorInputBackground string,
+	colorForeground string, colorCheckMark string, colorXMark string, colorLabel string) {
+	if colorTheme == "custom" {
+		customThemeFile, err := os.OpenFile("./static/css/color-theme.css", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer customThemeFile.Close()
+
+		_, err = customThemeFile.WriteString(":root {" +
+			"\n\t--page-background: #" + colorPageBackground + ";" +
+			"\n\t--input-background: #" + colorInputBackground + ";" +
+			"\n\t--foreground: #" + colorForeground + ";" +
+			"\n\t--check: #" + colorCheckMark + ";" +
+			"\n\t--x: #" + colorXMark + ";" +
+			"\n\t--label: #" + colorLabel + ";" +
+			"\n}")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		from, err := os.Open("./static/color-themes/" + colorTheme + ".css")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer from.Close()
+
+		to, err := os.OpenFile("./static/css/color-theme.css", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer to.Close()
+
+		_, err = io.Copy(to, from)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
